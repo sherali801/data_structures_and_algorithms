9eff0258aba66e4bd69a3bea41f8f80a4e98116a
Sher Ali <ali4friends80@gmail.com>
2016-10-25 20:39:25 +0500

    move the code of binary search to a function in file 02_binary_search.cpp

--- Lectures/C++/02_binary_search.cpp
+++ Lectures/C++/02_binary_search.cpp
@@ -2,28 +2,29 @@
 #include <conio.h>
 using namespace std;
 
-const int SIZE = 10;
-
-int main()
-{
-  int arr[SIZE] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-  int find;
+bool binarySearch(int * arr, int size, int key) {
+  static int start = 0;
+  int end = size - 1;
   int mid;
-  int start = 0;
-  int end = SIZE - 1;
   bool found = false;
-  cin >> find;
   while (!found && start <= end) {
     mid = (start + end) / 2;
-    if (find == arr[mid]) {
+    if (key == *(arr + mid)) {
       found = true;
-    } else if (find < arr[mid]) {
+    } else if (key < *(arr + mid)) {
       end = mid - 1;
     } else {
       start = mid + 1;
     }
   }
-  if (!found) {
+  return found;
+}
+
+int main()
+{
+  int arr[10] =  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  
+  if (!binarySearch(arr, 10, 7)) {
     cout << "Not Found\n";
   } else {
     cout << "Found\n";
