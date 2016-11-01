5c92c893f9a40a2c3dec9e02f604641ab1bc2499
Sher Ali <ali4friends80@gmail.com>
2016-11-01 22:57:36 +0500

    refactor Tower of Honai Code

--- Labs/C++/Lab 3/Task 2/main.cpp
+++ Labs/C++/Lab 3/Task 2/main.cpp
@@ -6,7 +6,7 @@ int minimumNumberOfMovesToMoveDiscs(int numberOfDiscs);//lab 3, task 2, part 2
 int minimumNumberOfMinutesToMoveOneDiscPerMinute(int numberOfDiscs);//lab 3, task 2, part 3
 double minimumNumberOfDaysToMoveOneDiscPerMinute(int numberOfDiscs);//lab 3, task 2, part 4
 double minimumNumberOfYearsToMoveOneDiscPerSecond(double numberOfSeconds);//lab 3, task 2, part 5
-void moveDiscsInMinimumNumberOfMoves(int numberOfDiscs, int fromPeg, int toPeg, int tempPeg);//lab 3, task 2, part 6
+void moveDiscsInMinimumNumberOfMoves(int numberOfDiscs, char fromPeg, char toPeg, char tempPeg);//lab 3, task 2, part 6
 
 int main()
 {
@@ -14,7 +14,7 @@ int main()
   cout << minimumNumberOfMinutesToMoveOneDiscPerMinute(7) << " Minutes\n";
   cout << minimumNumberOfDaysToMoveOneDiscPerMinute(15) << " Days\n";
   cout << minimumNumberOfYearsToMoveOneDiscPerSecond(18446744073709551615) << "\n";
-  moveDiscsInMinimumNumberOfMoves(8, 1, 3, 2);
+  moveDiscsInMinimumNumberOfMoves(8, 'A', 'C', 'B');
   _getch();
   return 0;
 }
@@ -39,11 +39,10 @@ double minimumNumberOfYearsToMoveOneDiscPerSecond(double numberOfSeconds) {
   return ((((numberOfSeconds / 60) / 60) / 24) / 365);
 }
 
-void moveDiscsInMinimumNumberOfMoves(int numberOfDiscs, int fromPeg, int toPeg, int tempPeg) {
-  if (numberOfDiscs > 0)
-  {
+void moveDiscsInMinimumNumberOfMoves(int numberOfDiscs, char fromPeg, char toPeg, char tempPeg) {
+  if (numberOfDiscs > 0) {
     moveDiscsInMinimumNumberOfMoves(numberOfDiscs - 1, fromPeg, tempPeg, toPeg);
-    cout << "Move a disc from peg " << fromPeg
+    cout << "Move disc " << numberOfDiscs <<" from peg " << fromPeg
       << " to peg " << toPeg << endl;
     moveDiscsInMinimumNumberOfMoves(numberOfDiscs - 1, tempPeg, toPeg, fromPeg);
   }
