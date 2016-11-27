67b94b30fbd6384055648a765f91b6cab3b5e243
Sher Ali <ali4friends80@gmail.com>
2016-11-27 14:43:59 +0500

    improve code for common elements in two sorted arrays

--- Lectures/C++/02_common_elements_in_sorted_array.cpp
+++ Lectures/C++/02_common_elements_in_sorted_array.cpp
@@ -2,34 +2,22 @@
 #include <conio.h>
 using namespace std;
 
-const int SIZE_A = 7;
-const int SIZE_B = 7;
-
-bool binarySearch(int * arr, int size, int num) {
-  static int start = 0;
-  int end = size - 1;
-  int mid;
-  bool found = false;
-  while (!found && start <= end) {
-    mid = (start + end) / 2;
-    if (num == *(arr + mid)) {
-      found = true;
-    } else if (num < *(arr + mid)) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
-  }
-  return found;
-}
-
 int main()
 {
+  const int SIZE_A = 7;
+  const int SIZE_B = 7;
   int A[SIZE_A] = { 13, 27, 35, 40, 49, 55, 59 };
   int B[SIZE_B] = { 17, 35, 39, 40, 55, 58, 60 };
-  for (int i = 0; i < SIZE_A; i++) {
-    if (binarySearch(B, SIZE_B, A[i])) {
+  int i = 0, j = 0;
+  while (i < SIZE_A && j < SIZE_B) {
+    if (A[i] < B[j]) {
+      i++;
+    } else if (A[i] > B[j]) {
+      j++;
+    } else {
       cout << A[i] << " ";
+      i++;
+      j++;
     }
   }
   _getch();
